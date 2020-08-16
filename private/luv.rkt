f50d2b56ebc51e62f969993007c11b674c3906d8
hkrish <hari@hkrish.com>
2020-08-17 01:14:44 +0200

    Fix: ensure the components are flonum

--- private/luv.rkt
+++ private/luv.rkt
@@ -40,15 +40,16 @@
          [Zr (unsafe-struct-ref illuminant/pcs 2)]
          [e (fl/ 216. 24389.)]
          [k (fl/ 24389. 27.)]
+         [l (fl l)]
          [y (if (fl> l (fl* k e))
                 (flexpt (fl/ (fl+ l 16.) 116.) 3.)
                 (fl/ l k))]
          [d0 (fl+ Xr (fl* 15. Yr) (fl* 3. Zr))]
          [u0 (fl/ (fl* 4. Xr) d0)]
          [v0 (fl/ (fl* 9. Yr) d0)]
-         [d (fl* y (fl- (fl/ (fl* 39. l) (fl+ v (fl* 13. l v0))) 5.))]
+         [d (fl* y (fl- (fl/ (fl* 39. l) (fl+ (fl v) (fl* 13. l v0))) 5.))]
          [b (fl* -5. y)]
-         [a (fl* (fl/ 1. 3.) (fl- (fl/ (fl* 52. l) (fl+ u (fl* 13. l u0))) 1.))]
+         [a (fl* (fl/ 1. 3.) (fl- (fl/ (fl* 52. l) (fl+ (fl u) (fl* 13. l u0))) 1.))]
          [x (fl/ (fl- d b) (fl- a (fl/ -1. 3.)))]
          [z (fl+ (fl* x a) b)])
     (xyz x y z)))
