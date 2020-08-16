7cc1f8aaf297594f49726dbb2f1271d8e3e15ad1
hkrish <hari@hkrish.com>
2020-08-16 21:19:44 +0200

    debug print helper

--- private/helpers.rkt
+++ private/helpers.rkt
@@ -2,9 +2,11 @@
 
 (require (for-syntax racket/base
                      racket/match)
-         racket/match)
+         racket/match
+         racket/pretty
+         racket/string)
 
-(provide define*)
+(provide define* dbg)
 
 (define-syntax (define* stx)
   (syntax-case stx ()
@@ -16,3 +18,17 @@
            (match*/derived
             (occ ...) (id mclause mclauses ...)
             ((mclause mclauses ...) body0 body ...)))))]))
+
+
+(define-syntax (dbg stx)
+  (syntax-case stx ()
+    [(_ f)
+     #`(call-with-values
+        (lambda () f)
+        (lambda res
+          (display "  : ")
+          (pretty-print 'f)
+          (displayln
+           (let ([lines (map (lambda (r) (pretty-format r #:mode 'print)) res)])
+             (string-join lines "     \n" #:before-first "  -> ")))
+          (apply values res)))]))
