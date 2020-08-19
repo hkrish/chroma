e584b614db058dced464b854594c41c507bc1d82
hkrish <hari@hkrish.com>
2020-08-19 22:46:53 +0200

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,8 +1,20 @@
 #lang info
+
 (define collection "chroma")
-(define pkg-desc "Library for working with colors, color palettes, and color space conversion.")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/chroma.scrbl" ())))
+
 (define version "0.5")
+
+(define pkg-desc "Library for working with colors, color palettes, and color space conversion.")
+
+(define deps '("base" "math-lib" "draw-lib" "pict-lib"))
+
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+
+(define scribblings '(("scribblings/chroma.scrbl" ())))
+
+(define compile-omit-paths '("test"))
+
+(define test-include-paths '("test"))
+(define test-omit-paths '("./info.rkt" "./main.rkt" "private" "scribblings"))
+
 (define pkg-authors '(hkrish))
